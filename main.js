c1b90cff580142fd22fc1f92436ce75d677e395c
Sergey <reliablesy@gmail.com>
2021-02-11 11:09:42 +0200

    LocalStorrage, create 1 element

--- main.js
+++ main.js
@@ -18,16 +18,76 @@ function save(list) {
     const listString = JSON.stringify(list);
     localStorage.setItem(LIST_SAVE_KEY, listString);
 }
-let container = document.getElementById('added');
+let localAdd = document.getElementById('localAdd');
 
 function f() {
     const i = localStorage.getItem(LIST_SAVE_KEY);
     const parseTodo = JSON.parse(i);
-    for (let todoItem of parseTodo) {
-        container.appendChild(todoItem.title);
-    }
-}
+    for (let todoItem of parseTodo.todos) {
+        const input = document.getElementById(`input-txt`);
+        const root = document.getElementById(`todo-txt`);
+        const wrapper = document.createElement(`div`);
 
+        const toggle = document.createElement(`input`);
+        const clean = document.createElement(`button`);
+
+        const completeText = document.createElement(`span`);
+        const todoText = document.createElement(`input`);
+        const tagImportant = document.createElement(`button`);
+
+        todoText.value = todoItem.title;
+        toggle.type = 'checkbox';
+        clean.innerHTML = `Удалить`;
+        input.value = '';
+        completeText.innerText = `Выполнено!`;
+        tagImportant.innerText = `Приоритет`;
+        tagImportant.className = "importantButton";
+
+        root.appendChild(wrapper);
+        wrapper.appendChild(todoText);
+        wrapper.appendChild(toggle);
+        wrapper.appendChild(clean);
+        wrapper.appendChild(tagImportant);
+
+        tagImportant.addEventListener(`click`, function () {
+            if (todoItem.buttonImportantCheck == false) {
+                tagImportant.innerText = `O`;
+            } else {
+                tagImportant.innerText = `Приоритет`;
+            }
+            todoItem.buttonImporatntCheck = !todoItem.buttonImportantCheck;
+        });
+
+        clean.addEventListener(`click`, function () {
+            wrapper.remove();
+            list.todos = list.todos.filter(value => todoItem !== value);
+            save(list);
+            localStorage.removeItem(LIST_SAVE_KEY);
+        });
+
+        function mark() {
+            todoText.style.textDecoration = `line-through`;
+            completeText.style.display = `block`;
+            todoText.before(completeText);
+        }
+
+        function unmark() {
+            completeText.style.display = `none`;
+            todoText.style.textDecoration = `none`;
+        }
+
+        toggle.addEventListener('click', () => {
+            if (todoItem.checked === true) {
+                unmark();
+            } else {
+                mark();
+            }
+            todoItem.checked = !todoItem.checked;
+        });
+    }
+
+}
+f();
 function add() {
 
     const input = document.getElementById(`input-txt`);
@@ -41,12 +101,10 @@ function add() {
     const todoText = document.createElement(`input`);
     const tagImportant = document.createElement(`button`);
 
-
-
     let todo = {
         title: input.value,
         checked: false,
-        buttonImporatntCheck: false
+        buttonImportantCheck: false
     };
 
     todoText.value = todo.title;
@@ -65,12 +123,12 @@ function add() {
 
 
     tagImportant.addEventListener(`click`, function () {
-        if (todo.buttonImporatntCheck == false) {
+        if (todo.buttonImportantCheck == false) {
             tagImportant.innerText = `O`;
         } else {
             tagImportant.innerText = `Приоритет`;
         }
-        todo.buttonImporatntCheck = !todo.buttonImporatntCheck;
+        todo.buttonImporatntCheck = !todo.buttonImportantCheck;
     });
 
     clean.addEventListener(`click`, function () {
